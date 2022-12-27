8cf4c6c43de242369920fc574f376cf54407f7c8
Kanatbek-AKA <61031156+Kanatbek-AKA@users.noreply.github.com>
2022-12-27 11:44:32 +0100

    Update test_routes.py

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -186,3 +186,4 @@ class TestAccountService(TestCase):
         res = self.client.get("/", environ_overrides=HTTPS_ENVIRON)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.headers.get('Access-Control-Allow-Origin'), '*')
+        # end of codeline
