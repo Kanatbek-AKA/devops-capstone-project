0d1b35f41f56b344a24a2d7beb70368b1503bef2
Kanatbek-AKA <61031156+Kanatbek-AKA@users.noreply.github.com>
2022-12-27 08:45:17 +0100

    Update test_cli_commands.py

--- tests/test_cli_commands.py
+++ tests/test_cli_commands.py
@@ -17,8 +17,7 @@ class TestFlaskCLI(TestCase):
     @patch('service.common.cli_commands.db')
     def test_db_create(self, db_mock):
         """It should call the db-create command"""
-        db_mock.return_value = MagicMock()
-        
+        db_mock.return_value = MagicMock()        
         with patch.dict(os.environ, {"FLASK_APP": "service:app"}, clear=True):
             result = self.runner.invoke(db_create) # to understand background of this line code 
-            self.assertEqual(result.exit_code, 0)  #    
+            self.assertEqual(result.exit_code, 0)  
